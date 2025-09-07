125c527250e613e425145df9c5fb5c6701e3b5a5
Eduardo F <efernandez19980@gmail.com>
2025-09-07 10:23:50 -0400

    fix bag vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/EfernandezOrtiz/",,
+  base: "/EfernandezOrtiz/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
